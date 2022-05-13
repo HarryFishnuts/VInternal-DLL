045708fef8ec808b49ca326a79a11b694f78a411
JT <bailey.jt.brown@gmail.com>
2022-05-12 20:04:15 -0400

    Working on it...

--- buffers.c
+++ buffers.c
@@ -7,14 +7,22 @@
  * Contents:
  *	- Includes
  *	- Internal data
+ *	- Internal allocation functions
  *	- Internal init and terminate functions
  *	- Buffer related functions
  * ==================== */
 
 /* INCLUDES */
-#include "internals.h"
+#include <stdio.h>   /* Basic I/O         */
+#include <stdlib.h>  /* Memory Management */
+#include <Windows.h> /* WIN32 Calls       */
+
+#include "internals.h" /* Source Header */
+
 
 /* INTERNAL DATA */
+static HANDLE viHeap;
+
 static viBufferBehavior* viBufferBehaviors; /* buffer behavior array */
 static uint16_t viBufferBehaviorCount;      /* behavior counter      */
 
@@ -22,8 +30,48 @@ static viBuffer** viBuffers;     /* pointer to buffer objects */
 static uint16_t   viBufferCount; /* buffer object counter     */
 static uint8_t*   viBufferField; /* buffer use field          */
 
+/* INTERNAL ALLOCATION FUNCTIONS */
+static void* viAlloc(size_t numBytes)
+{
+	return HeapAlloc(viHeap, FALSE, numBytes);
+}
+
+static void* viAllocZ(size_t numBytes)
+{
+	return HeapAlloc(viHeap, HEAP_ZERO_MEMORY, numBytes);
+}
+
+static void viFree(void* block)
+{
+	HeapFree(viHeap, FALSE, block);
+}
+
 /* INTERNAL INIT AND TERMINATE FUNCTIONS */
-VIAPI void __viBufferInit(void);
+VIAPI void __viBufferInit(void)
+{
+	/* get system info on page size */
+	SYSTEM_INFO sysInfo;
+	GetSystemInfo(&sysInfo);
+	int pageSize = sysInfo.dwPageSize;
+
+	/* get the amount of bytes for 2 pages */
+	int pageSizeRequest = pageSize * VI_HEAP_PAGEALLOC;
+
+	/* create heap */
+	viHeap = HeapCreate(0, pageSizeRequest, 0);
+
+	/* clear internal counters */
+	viBufferCount = 0;
+	viBufferBehaviorCount = 0;
+
+	/* allocate internal data */
+	viBufferBehaviors = viAlloc(sizeof(viBufferBehavior) *
+		VI_BUFFERBEHAVIORS_MAX);
+	viBuffers = viAlloc(sizeof(viBuffer*) * VI_BUFFERS_MAX);
+	viBufferField = viAlloc(sizeof(uint8_t) * VI_BUFFERS_MAX);
+
+}
+
 VIAPI void __viBufferTerminate(void);
 
 /* BUFFER FUNCTIONS */
